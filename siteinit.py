d84a41bf9d58304bbaa7fa4b09dc2e35f3cf7333
komoto <komoto@jeol.co.jp>
2021-05-27 11:53:36 +0900

    Update si and eggs

--- siteinit.py
+++ siteinit.py
@@ -36,6 +36,7 @@ def init_frame(self):
     ## Load plugins
     ## --------------------------------
     sys.path.append(r"C:\usr\home\workspace\tem13\gdk-aero")
+    sys.path.append(r"C:\usr\home\workspace\tem13\gdk-data")
     
     self.edi = self.require("editor")
     self.su = self.require('startup')
