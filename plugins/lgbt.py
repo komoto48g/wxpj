4b71b83279359ff987ef808368b282781175ae9f
komoto <komoto@jeol.co.jp>
2021-06-23 17:01:35 +0900

    Change lgbt panel (hide hi,lo param)

--- plugins/lgbt.py
+++ plugins/lgbt.py
@@ -34,7 +34,7 @@ class Plugin(Layer):
         
         self.layout("blur-threshold", self.params, type='vspin', cw=0, lw=40, tw=40)
         self.layout("cutoff [%]", self.cutoff_params,
-                    row=1, show=0, type='vspin', cw=-1, lw=16, tw=44)
+                    row=1, show=0, visible=0, type='vspin', cw=-1, lw=16, tw=44)
         self.layout(None, [btn], row=2)
     
     ksize = property(lambda self: self.params[0])
