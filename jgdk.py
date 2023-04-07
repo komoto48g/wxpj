276aed0636668bc4a0ba2fab83362afdb9d7a6eb
Kazuya O'moto <komoto@jeol.co.jp>
2023-04-07 20:53:58 +0900

    Update egg for PY310

--- jgdk.py
+++ jgdk.py
@@ -11,8 +11,14 @@ from mwx.graphman import Frame, Layer, Thread, Graph
 from mwx.controls import Param, LParam, Knob, Icon, Clipboard
 from mwx.controls import Button, ToggleButton, TextCtrl, Choice, Gauge, Indicator
 
+## for debug
+## sys.path.append(r"C:\usr\home\workspace\tem13\gdk-packages")
+
 home = os.path.dirname(os.path.abspath(__file__))
-if sys.version_info >= (3,8):
+
+if sys.version_info >= (3,10):
+    eggs = os.path.join(home, "nest/*-py3.10.egg")
+elif sys.version_info >= (3,8):
     eggs = os.path.join(home, "nest/*-py3.8.egg")
 else:
     eggs = os.path.join(home, "nest/*-py3.5.egg")
