dbd8f2922fb74eeb308431c24208115511c202fa
Kazuya O'moto <komoto@jeol.co.jp>
2023-07-30 22:53:25 +0900

    Update docs of grid/lccf Update lgbt
    Remove btn1

--- plugins/pragma_grid.py
+++ plugins/pragma_grid.py
@@ -9,8 +9,9 @@ class Plugin(Layer):
     """Evaluate distortion of Mag-Grid pattern.
     
     Run the following steps:
-    1. lccf.run to find contours of circles
-    2. ld_grid.run to calc the aspect ratio
+    
+    1. :meth:`plugins.lccf.Plugin.run` to find contours of circles
+    2. :meth:`plugins.ld_grid.Plugin.run` to calc the aspect ratio
     """
     menukey = "Plugins/&Pragma Tools/"
     category = "Pragma Tools"
@@ -35,11 +36,13 @@ class Plugin(Layer):
         )
     
     def show_setting(self, force=0):
+        """Show the settings."""
         b = force or not self.ld.IsShown()
         self.ld.Show(b)
         self.lccf.Show(1)
     
     def run(self):
+        """Run scripts."""
         self.lccf.run(otsu=True, invert=self.chk.Value)
         if self.chkfit.Value:
             self.ld.thread.Start(self.ld.run)
