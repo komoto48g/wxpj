6fc8789f55677828c4f5b8369f9c610ee5934cb3
Kazuya O'moto <komoto@jeol.co.jp>
2024-03-28 01:20:46 +0900

    Revert "Move jgdk --> wxpj"
    was too aggressive

--- jgdk.py
+++ jgdk.py
@@ -1,2 +1,5 @@
 #! python3
-from wxpyJemacs import *
+from mwx.framework import Menu, StatusBar # noqa
+from mwx.graphman import Frame, Layer, Thread, Graph # noqa
+from mwx.controls import Param, LParam, Knob, Icon, Icon2, Clipboard # noqa
+from mwx.controls import Button, ToggleButton, TextCtrl, Choice, Gauge, Indicator # noqa
--- wxpyJemacs.py
+++ wxpyJemacs.py
@@ -24,10 +24,7 @@ import wx
 import wx.adv
 import numpy as np
 
-from mwx.framework import Menu, StatusBar # noqa
-from mwx.controls import Param, LParam, Knob, Icon, Icon2, Clipboard # noqa
-from mwx.controls import Button, ToggleButton, TextCtrl, Choice, Gauge, Indicator # noqa
-from mwx.graphman import Frame, Layer, Thread, Graph # noqa
+from mwx.graphman import Frame
 
 HOME = os.path.dirname(__file__)
 eggs = r"nest/*-py{}.{}.egg".format(*sys.version_info)
