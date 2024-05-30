e37bfac77f14414da641fa83fc920d99c3aaec52
Kazuya O'moto <komoto@jeol.co.jp>
2024-05-30 16:08:48 +0900

    Update pragma_grid/pragma_ring

--- plugins/pragma_ring.py
+++ plugins/pragma_ring.py
@@ -11,31 +11,37 @@ class Plugin(Layer):
     category = "Pragma Tools"
     caption = "Ring"
     
+    lgbt = property(lambda self: self.parent.require('lgbt'))
     lcrf = property(lambda self: self.parent.require('lcrf'))
     ld = property(lambda self: self.parent.require('ld_ring'))
     
     def Init(self):
-        self.chkfit = wx.CheckBox(self, label="fit")
-        self.chkfit.Value = True
-        
         self.layout((
-                Button(self, "Run", self.run),
-                self.chkfit,
-                (),
+                Button(self, "1. Mark", self.calmark, icon='help', size=(72,-1)),
+                self.lcrf.rmin,
+                
+                Button(self, "2. Run", self.run, icon='help', size=(72,-1)),
                 Button(self, "Setting", self.show_setting),
             ),
-            row=3,
+            title="Evaluate ring pattern", row=2,
+            type='vspin', cw=-1, lw=0, tw=44,
         )
+        self.lgbt.ksize.value = 5 # default blur window size
     
-    def show_setting(self, force=0):
+    def show_setting(self):
         """Show the settings."""
-        b = force or not self.ld.IsShown()
-        self.ld.Show(b)
-        self.lcrf.Show(1)
+        self.lcrf.Show()
     
     def run(self):
-        """Run scripts."""
-        self.lcrf.run()
-        if self.chkfit.Value:
-            self.ld.thread.Start(self.ld.run)
-            self.show_setting(1)
+        """Run the fitting procedure."""
+        frame = self.graph.frame
+        self.ld.thread.Start(self.ld.run, frame)
+        self.ld.Show()
+    
+    def calmark(self, frame=None):
+        """Feature detection.
+        
+        Set parameter: Minimum radius [%] of rings to be detected.
+        """
+        frame = self.graph.frame
+        self.lcrf.run(frame)
