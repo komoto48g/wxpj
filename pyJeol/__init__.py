90bf9f0cd5443091ca91a2b50d51eefa1fa9362c
Kazuya O'moto <komoto@jeol.co.jp>
2024-06-24 23:07:31 +0900

    Move pyJeol.detector downstream --> src/jeolcamera; Update Jeol eggs

--- pyJeol/__init__.py
+++ pyJeol/__init__.py
@@ -1,6 +1,9 @@
 #! python3
 """pyJeol package
 """
+## from .src.jeolcamera import Camera, DummyCamera
+from ._jeolcamera import Camera, DummyCamera
+
 from .temisc import Environ
 from .temisc import mrange
 from .temisc import FLHex, OLHex
